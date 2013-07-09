652811286a350e999a0818efd0b42b76ef1d75e8
Jack Spenser <jack.spenser@gmx.us>
2013-07-08 19:59:40 -0500

    Add unique constraint to user_roles many to many table

--- taskplanner/model.py
+++ taskplanner/model.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app)
 user_roles = db.Table('user_roles',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
     db.Column('role_id', db.Integer, db.ForeignKey('roles.id')),
+    db.UniqueConstraint('user_id', 'role_id', name='uix_user_role')
     )
 
 class User(db.Model):
