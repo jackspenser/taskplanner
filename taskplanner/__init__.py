e8113e263e76bdbab1858243ac2070cae617c31d
jackspenser <jack.spenser@gmx.us>
2014-02-11 08:18:27 -0600

    Change to PostgresConfig

--- taskplanner/__init__.py
+++ taskplanner/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object('taskplanner.config.DevelopmentConfig')
-#app.config.from_object('taskplanner.config.PostgresConfig')
+#app.config.from_object('taskplanner.config.DevelopmentConfig')
+app.config.from_object('taskplanner.config.PostgresConfig')
 
 import taskplanner.views.admin
 import taskplanner.views.auth
